8b7817d15062827477b81a08cc2c54e38f51ab91
Florian Oppermann <florian.oppermann@eonerc.rwth-aachen.de>
2022-06-15 13:47:35 +0000

    fix database interaction
    Signed-off-by: Florian Oppermann <florian.oppermann@eonerc.rwth-aachen.de>

--- app/routers/v2/models/model_controller.py
+++ app/routers/v2/models/model_controller.py
@@ -1,4 +1,4 @@
-import json
+import pickle
 
 from app.common.database import redis_model, redis_job, get_unique_id
 from app.schemas.v2.model import PredModelBase, PredModel, PredModelCreationJob
@@ -10,15 +10,15 @@ async def get_all_model_ids():
     return [key.decode("utf-8").split("_")[-1] for key in keys]
 
 async def get_model(id:int):
-    model = await redis_model.get(f"model_{id}")
-    return PredModel(**json.loads(model))
+    model = pickle.loads(await redis_model.get(f"model_{id}"))
+    return model
 
 
 async def create_model(data:PredModelBase):
     id = await get_unique_id()
     # model = PredModel(**data.dict(), model_id = mode_id)
     job = PredModelCreationJob(job_id=id,resource=data)
-    await redis_job.set(f"model_{id}",job.json())
+    res = await redis_job.set(f"model_{id}",job.json())
     await amqp_publisher.publish(f"model.{id}", job.dict())
     return job
 
