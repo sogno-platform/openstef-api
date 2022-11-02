639a2b9abe33174fbe6d60561f5de214a931722b
Florian Oppermann <florian.oppermann@eonerc.rwth-aachen.de>
2022-11-02 13:23:23 +0000

    remove worker from image
    Signed-off-by: Florian Oppermann <florian.oppermann@eonerc.rwth-aachen.de>

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from alliander_buildtools_python.build_libs import build_pkg
 
 build_pkg(
     setup_file=__file__,
-    author="Korte Termijn Prognoses",
-    author_email="korte.termijn.prognoses@alliander.com",
+    author="",
+    author_email="",
     description="YOURDESCRIPTION",
     url="YOURPORJECTGIRURL.git",
 )
